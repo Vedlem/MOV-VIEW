377c14ccc0f84811942b398efe2d603c4a49c770
Vedlem <vedlem.mail@gmail.com>
2024-10-09 06:06:27 +0200

    Message descriptif des modifications

--- script.js
+++ script.js
@@ -1,50 +1,16 @@
-fetch('config.json')
-    .then(response => response.json())
-    .then(config => {
-        fetchMovies(config);
-    })
-    .catch(error => console.error('Erreur lors du chargement de la configuration:', error));
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const traktUrl = 'https://trakt.tv/users/vedlem2/lists/vus';
 
-async function fetchMovies(config) {
-    try {
-        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        const traktUrl = config.listUrl;
-
-        const response = await fetch(proxyUrl + traktUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'trakt-api-version': '2',
-                'trakt-api-key': config.clientId
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des données');
-        }
-
-        const data = await response.json();
-        displayMovies(data);
-    } catch (error) {
-        console.error('Erreur :', error);
+fetch(proxyUrl + traktUrl, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': config.clientId
     }
-}
-
-function displayMovies(movies) {
-    const movieGrid = document.getElementById('movieGrid');
-    movieGrid.innerHTML = '';
-
-    movies.forEach(item => {
-        const movie = item.movie;
-        const movieDiv = document.createElement('div');
-        movieDiv.classList.add('movie');
-
-        movieDiv.innerHTML = `
-            <img src="${movie.images.poster.thumb}" alt="${movie.title}">
-            <h3>${movie.title}</h3>
-            <p>Année : ${movie.year}</p>
-        `;
-
-        movieGrid.appendChild(movieDiv);
-    });
-}
+})
+.then(response => response.json())
+.then(data => {
+    displayMovies(data);
+})
+.catch(error => console.error('Erreur lors de la récupération des données via le proxy :', error));
